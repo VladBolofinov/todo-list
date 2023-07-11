5969adf327db3c7566d9d18e88678d7ccc839345
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-07-11 20:36:34 +0300

    made logic create task and solved task

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,66 +1,3 @@
-export const onLoadingData = (loading) => {
-    return {
-        type: 'ON_LOADING_DATA',
-        payload: loading
-    }
-}
-
-export const onTotalVacancies = (number) => {
-    return {
-        type: 'TOTAL_VACANCIES',
-        payload: number
-    }
-}
-
-export const onEmptyPage = (value) => {
-    return {
-        type: 'ON_EMPTY_PAGE',
-        payload: value
-    }
-}
-
-export const onAddCategories = (categories) => {
-    return {
-        type: 'ADD_CATEGORIES',
-        payload: categories
-    }
-}
-
-export const onAddCategoriesKeys = (keys) => {
-    return {
-        type: 'ADD_CATEGORIES_KEYS',
-        payload: keys
-    }
-}
-
-export const onAddVacancyList = (vacancies) => {
-    return {
-        type: 'ADD_VACANCY_LIST',
-        payload: vacancies
-    }
-}
-
-export const onAddProfNameValue = (value) => {
-    return {
-        type: 'ADD_PROFNAME_VALUE',
-        payload: value
-    }
-}
-
-export const onAddSalaryFrom = (salary) => {
-    return {
-        type: 'ADD_SALARY_FROM',
-        payload: salary
-    }
-}
-
-export const onAddSalaryTo = (salary) => {
-    return {
-        type: 'ADD_SALARY_TO',
-        payload: salary
-    }
-}
-
 export const onAddInputValue = (value) => {
     return {
         type: 'ADD_INPUT_VALUE',
@@ -68,9 +5,16 @@ export const onAddInputValue = (value) => {
     }
 }
 
-export const onAddKeyValue = (value) => {
+export const onAddTask = (value) => {
     return {
-        type: 'ADD_KEY_VALUE',
+        type: 'ADD_TASK',
         payload: value
+    }
+}
+
+export const onActiveTask = (id) => {
+    return {
+        type: 'TASK_STATUS',
+        payload: id
     }
 }
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,20 +3,26 @@ import {useEffect, useState} from "react";
 import './App.scss';
 
 import {useDispatch, useSelector} from 'react-redux';
-import {} from "../../actions";
-//1) Создание задачи: Пользователь должен иметь возможность добавлять новые задачи в список Todo.
+import {onAddInputValue,onAddTask,onActiveTask} from "../../actions";
+//1) Создание задачи: Пользователь должен иметь возможность добавлять новые задачи в список Todo. (сделано)
 //2) Редактирование задачи: Пользователь должен иметь возможность редактировать текст задачи.
 //3) Удаление задачи: Пользователь должен иметь возможность удалять задачи из списка.
-//4) Пометка задачи как выполненной: Пользователь должен иметь возможность пометить задачу как выполненную или снять пометку.
+//4) Пометка задачи как выполненной: Пользователь должен иметь возможность пометить задачу как выполненную или снять пометку.(сделано)
 export const App = () => {
     const dispatch = useDispatch();
-    const {} = useSelector(state => state);
+    const {inputValue,tasks} = useSelector(state => state);
 
+    const tasksRender = tasks.map(item => (
+        <div className='task-wrapper' key={item.id} onClick={()=>console.log(item.id)}>
+            <input type="checkbox" onClick={() => dispatch(onActiveTask(item.id))}/>
+            <p>{item.id}) --- {item.descr}</p>
+        </div>
+    ));
     return (
         <div className="App">
-            <input type="text"/>
-            <button>Add task</button>
-            <p className='task'>Buy coffee tomorrow!</p>
+            <input type="text" onChange={(e)=>dispatch(onAddInputValue(e.currentTarget.value))}/>
+            <button onClick={()=>dispatch(onAddTask({id:tasks.length + 1, descr:`${inputValue}`, active:false}))}>Add task</button>
+            {tasksRender}
         </div>
     );
 }
